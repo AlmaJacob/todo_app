4d070227fc2bb245a899a9622f3a0aa11da4e012
almajacob <almajacob7@gmail.com>
2025-01-08 13:27:35 +0530

    added

--- todo_api/todo_api/settings.py
+++ todo_api/todo_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-pv@6rjyk@dup@iqf0lwy&3^1c-0i1+83e4nn&(q5s!4e*&&yvj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 CORS_ALLOWED_ORIGINS=[
-    "https://localhost:5173"
+    "http://localhost:5173"
 ]
 
 ALLOWED_HOSTS = []
