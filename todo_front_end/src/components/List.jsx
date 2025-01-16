60284a7f964351090773c871b83422bf88809882
almajacob <almajacob7@gmail.com>
2025-01-16 13:43:06 +0530

    added

--- todo_front_end/src/components/List.jsx
+++ todo_front_end/src/components/List.jsx
@@ -1,24 +1,33 @@
-import { useEffect,useState } from "react"
+import { useEffect, useState } from "react"
 import axios from 'axios'
+import Add  from "./Add"
+
 function List(){
-    const[data,setData]=useState([])
-    const[editing,setEditing]=useState(false)
-    const[editdata,setEditData]=useState(null)
+    const [data,setData] = useState([])
+    const [editing,setEditing] = useState(false)
+    const [editdata,setEditData] = useState(null)
     useEffect(()=>{
         axios.get('http://127.0.0.1:8000/api/todo/').then((res)=>{
-             console.log(res.data);
-             setData(res.data)
-
+            console.log(res.data);
+            setData(res.data)
         }).catch(error=>console.log(error.message))
+        
     },[])
-    const Edit_dtls = (task)=>{
+    const Edit_dtls = (task) =>{
         setEditing(true)
         setEditData(task)
     }
-    const Delete_dtls=(id) =>{
-        axios.delete('http://127.0.0.1:8000/api/todo/').then((res)=>{
-            setData(data.filter((task)=>task.id))
+    const updateDtls = (id,task) =>{
+        setEditing(false)
+        axios.put(`http://127.0.0.1:8000/api/todo/${id}/`,task).then(res =>{
+            setData(data.map((prv)=> prv.id==id ? res.data : prv))
+        }).catch(error =>console.log(error.message))
+    }
+    const Delete_dtls = (id) =>{
+        axios.delete(`http://127.0.0.1:8000/api/todo/${id}/`).then(res=>{
+            setData(data.filter((task)=>task.id!=id))
         }).catch(error=>console.log(error.message))
+
     }
     return(
         <div className="container">
@@ -28,7 +37,6 @@ function List(){
                     <tr>
                         <th>Task</th>
                         <th>Description</th>
-                        
                     </tr>
                 </thead>
                 <tbody>
@@ -36,25 +44,33 @@ function List(){
                         <tr key={index}>
                             <td>{value.task}</td>
                             <td>{value.description}</td>
-                            <td><button className="btn btn-outline-info" onClick={()=>{Edit_dtls(value)}}>edit</button></td>
-                            <td><button className="btn btn-outline-danger" onClick={()=>{}}>delete</button></td>
+                            <td><button className="btn btn-outline-info" onClick={()=>{Edit_dtls(value)}}>Edit</button></td>
+                            <td><button className="btn btn-outline-danger" onClick={()=>{Delete_dtls(value.id)}}>Delete</button></td>
+
                         </tr>
                     ))}
-
                 </tbody>
             </table>
-            {editing ? <EditForm curTask={editdata}/>:null}
+            {editing ? <EditForm curTask={editdata} updatefun={updateDtls}/>:<Add/>}
         </div>
     )
 }
-const EditForm = ({curTask})=>{
+const EditForm = ({curTask,updatefun})=>{
     const [task,setTask] = useState(curTask)
-    return(
-        <form>
-            <input type="text" name="title" id="title" value={task.task} />
-            <input type="text" name="description" id="description" value={task.description} />
-            <input type="submit" value="update"/>       
+    const handleChange =(e) => {
+        const {name,value} = e.target
+        setTask({...task,[name]:value})
+    }
+    const handleSubmit = (e) => {
+        e.preventDefalut()
+        updatefun(task.id,task)
+    }
+    return (
+        <form onSubmit={handleSubmit}>
+            <input type="text" name="task" id="title" value={task.task} onChange={handleChange}/>
+            <input type="text" name="description" id="description" value={task.description} onChange={handleChange}/>
+            <input type="submit" value="Update" />
         </form>
     )
 }
- export default List
+export default List
